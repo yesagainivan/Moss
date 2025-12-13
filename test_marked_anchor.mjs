225d9f9018c12e34e03ff479fe99258e5af54201
yesagainivan <brandonowono@gmail.com>
2025-12-13 14:47:19 -0500

    More cleaning

--- test_marked_anchor.mjs
+++
@@ -1,3 +0,0 @@
-import { marked } from 'marked';
-const input = 'The [ _Schools Summary](#wikilink:Austria%2FSchools%2F_Schools%20Summary%20) note is not fully up to date.';
-console.log(marked(input));
